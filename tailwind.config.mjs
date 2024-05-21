dcd3ccc62eb55595abbb73149367f94fb863d3fe
CaperAsDev <caper.asdev@gmail.com>
2024-05-21 13:27:46 -0500

    Layout done, next: add product images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				montserrat: ["Montserrat", "sans-serif"],
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				accent: "var(--color-accent)",
+			},
+			keyframes: {
+				carousel: {
+					'0%': { transform: 'translateX(0%)'},
+					'10%': { transform: 'translateX(0%)'},
+					'90%': { transform: 'translateX(-100%)'},
+					'100%': { transform: 'translateX(-100%)'}
+				}
+			},
+			animation: {
+				carousel: 'carousel 10s linear alternate infinite'
+			}
+		},
 	},
 	plugins: [],
 }
