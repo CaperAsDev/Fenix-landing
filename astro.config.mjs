711e896302dc8c493a1f74b9e4e96df33b1c4566
CaperAsDev <caper.asdev@gmail.com>
2024-05-25 13:33:22 -0500

    add compressor integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
+    tailwind(), 
     icon({
-      iconDir: "src/assets/icons",
-    })],
-    base: process.env.SERVER === 'pre-production' ? `/${process.env.REPO}` : '/',
-    build: {
-      assets: 'assets'
-    }
+      iconDir: "src/assets/icons"
+    }),
+    playformCompress()
+  ],
+  base: process.env.SERVER === 'pre-production' ? `/${process.env.REPO}` : '/',
+  build: {
+    assets: 'assets'
+  }
 });
