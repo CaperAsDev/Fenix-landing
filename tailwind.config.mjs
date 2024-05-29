cee73465a3a49702332c8c86392b2f11164ad29c
CaperAsDev <caper.asdev@gmail.com>
2024-05-29 12:29:01 -0500

    Done?

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,17 +10,6 @@ export default {
 				primary: "var(--color-primary)",
 				secondary: "var(--color-secondary)",
 				accent: "var(--color-accent)",
-			},
-			keyframes: {
-				carousel: {
-					'0%': { transform: 'translateX(0%)'},
-					'10%': { transform: 'translateX(0%)'},
-					'90%': { transform: 'translateX(-100%)'},
-					'100%': { transform: 'translateX(-100%)'}
-				}
-			},
-			animation: {
-				carousel: 'carousel 10s linear alternate infinite'
 			}
 		},
 	},
