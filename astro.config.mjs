62581598ee9a89a94efd123dc0744fce6a540baf
CaperAsDev <caper.asdev@gmail.com>
2024-06-17 10:20:01 -0500

    adding sitemap and removin pre-production env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import playformCompress from "@playform/compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    icon({
-      iconDir: "src/assets/icons"
-    }),
-    playformCompress()
-  ],
+  site: "https://manufacturasfennixx.com/",
+  integrations: [tailwind(), icon({
+    iconDir: "src/assets/icons"
+  }), playformCompress(), sitemap()],
   base: process.env.SERVER === 'pre-production' ? `/${process.env.REPO}` : '/',
   build: {
     assets: 'assets'
